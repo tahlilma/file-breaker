f80a2ab8b4bafeb4d69037a297d86c923d6e35d2
tahlilma <admimterminal2000@gmail.com>
2021-06-30 12:33:57 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# File Breaker
+A simple CLI that 'corrupts' your files. It works by just adding fake binary to your files. Unless its a text file or any type of file that is text based it should succesfully corrupt it. 
+
+## Installation
+Just run (May require sudo or admin perms)
+```
+npm install -g file-breaker
+```
---
+++ src/index.ts
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+import fs from 'fs';
+
+const args: string[] = process.argv;
+const fileName: string = args[2];
+const filePath: string = `${process.cwd()}/${fileName}`;
+
+const payload: number[] = Array.from({ length: 2**12 }, (): number => {
+    return Math.floor(Math.random() * (1 - 0 + 1));
+})
+
+/**
+ * Corrupts the file given to it.
+ *  
+ * @param path - The path to the file which is to be corrupted
+ * @returns - nothing
+*/
+const corruptor = (path: string): void => {
+    try {
+        if (fs.readFileSync(path)) {
+            const fileData: string = fs.readFileSync(path, 'utf8');
+            const writeData: string = fileData + payload.toString();
+            fs.writeFileSync(path, writeData);
+            console.log('\x1b[32m', `${path} Corrupted Succesfully :)`);
+        }  
+    } catch (err) {
+        console.log('\x1b[31m', err);
+    }
+}
+
+if (fileName === undefined) {
+    console.log('\x1b[31m', '\nPlease pass in a file as an argument');
+    process.exit(1);
+} else {
+    corruptor(filePath);
+}
